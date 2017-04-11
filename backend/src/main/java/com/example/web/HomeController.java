c6c9372d05090ff4775358fddca7ac4c3560bd55
boroda4436 <chupika.bohdan@ukr.net>
2017-04-11 23:06:58 +0300

    initial commit

---
+++ backend/src/main/java/com/example/web/HomeController.java
@@ -0,0 +1,11 @@
+package com.example.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Bohdan on 11.04.2017
+ */
+//TODO add controller
+public class HomeController {
+
+}
