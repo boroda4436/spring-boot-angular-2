ca0d36665f4f27ed0e0637e41c054f7903d0be55
boroda4436 <chupika.bohdan@ukr.net>
2017-04-17 21:11:32 +0300

    start working with routing

--- frontend/src/main/frontend/src/app/app.component.ts
+++ frontend/src/main/frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HomeController } from "../providers/home-controller";
+import { TemperatureData } from "../models/TemperatureData";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  temperature = new TemperatureData();
+
+  constructor(public homeController: HomeController){}
   title = 'app works!';
+
+  loadTemperature() {
+    this.homeController.loadTemperature().subscribe(temp =>{
+      console.log("Temperature is: ", temp);
+      this.temperature = temp;
+    }, err => {console.log("Error while get temperature: ", err)});
+  };
 }
+
+
